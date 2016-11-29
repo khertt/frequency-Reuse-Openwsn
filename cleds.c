512529fcc12fc3196b7f61e85b1aaec5b8c13ab7
khertt <kv.mendez@gmail.com>
2016-11-29 05:56:54 -0300

    Update cleds.c
    enviar data

--- cleds.c
+++ cleds.c
@@ -65,7 +65,7 @@ void cleds__init() {
 owerror_t cleds_receive(
       OpenQueueEntry_t* msg,
       coap_header_iht*  coap_header,
-      coap_option_iht*  coap_options
+      coap_option_iht*  coap_options4
    ) {
    owerror_t outcome;
    
@@ -76,37 +76,120 @@ owerror_t cleds_receive(
          msg->length                      = 0;
          
          // add CoAP payload
-         packetfunctions_reserveHeaderSize(msg,13);
-         msg->payload[0]                  = COAP_PAYLOAD_MARKER;
+         //packetfunctions_reserveHeaderSize(msg,12);
+         //msg->payload[0]                  = COAP_PAYLOAD_MARKER;
 
-         if (leds_error_isOn()==1) {
-            
-            msg->payload[1]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[0];
-	    msg->payload[2]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[1];
-	    msg->payload[3]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[2];
-            msg->payload[4]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[3];
-	    msg->payload[5]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[4];
-	    msg->payload[6]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[5];
-	    msg->payload[7]               =schedule_vars.scheduleBuf[4].neighbor.addr_64b[6];
-	    msg->payload[8]               =schedule_vars.scheduleBuf[4].neighbor.addr_64b[7];
-	    msg->payload[9]               =schedule_vars.scheduleBuf[4].neighbor.addr_64b[8];
+         //if (leds_error_isOn()==1) {
+         uint8_t aux=0;
+         uint8_t y=0;
+         uint8_t x=0;
+         uint8_t i=4;
+         uint8_t s=4;
+   
+
+         	
+         
+        while(TRUE){
+
+		if (schedule_vars.scheduleBuf[i].type==1 || schedule_vars.scheduleBuf[i].type==2 ){
+			i=i+1;
+
+		}
+	        else{
+			break;		
+		}
+                if(i==10){
+			break;}
+	 }
+         
+         x=13*(i-4);
+         x=x+1;
+	 packetfunctions_reserveHeaderSize(msg,x);
+         
+         msg->payload[0]                  = COAP_PAYLOAD_MARKER;       
+
+         while(TRUE){
+
+	 
+		msg->payload[y+1]               = schedule_vars.scheduleBuf[s].neighbor.addr_64b[0];
+	 	msg->payload[y+2]               = schedule_vars.scheduleBuf[s].neighbor.addr_64b[1];
+	 	msg->payload[y+3]               = schedule_vars.scheduleBuf[s].neighbor.addr_64b[2];
+         	msg->payload[y+4]               = schedule_vars.scheduleBuf[s].neighbor.addr_64b[3];
+	 	msg->payload[y+5]               = schedule_vars.scheduleBuf[s].neighbor.addr_64b[4];
+	 	msg->payload[y+6]               = schedule_vars.scheduleBuf[s].neighbor.addr_64b[5];
+	 	msg->payload[y+7]               = schedule_vars.scheduleBuf[s].neighbor.addr_64b[6];
+	 	msg->payload[y+8]               = schedule_vars.scheduleBuf[s].neighbor.addr_64b[7];
+	 	msg->payload[y+9]               = schedule_vars.scheduleBuf[s].neighbor.addr_64b[8];
+	 	msg->payload[y+10]              = schedule_vars.scheduleBuf[s].type;
+	 	msg->payload[y+11]		  = schedule_vars.scheduleBuf[s].channelOffset;
+	 	msg->payload[y+12]		  = schedule_vars.scheduleBuf[s].slotOffset;
+		msg->payload[y+13]		  ='T';
+
+
+		s=s+1;
+		y=y+13;
+                if(s>i){
+			break;
+		}
+	
+                 
+	 
+	}
+
+
+	/* msg->payload[1]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[0];
+	 msg->payload[2]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[1];
+	 msg->payload[3]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[2];
+         msg->payload[4]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[3];
+	 msg->payload[5]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[4];
+	 msg->payload[6]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[5];
+	 msg->payload[7]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[6];
+	 msg->payload[8]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[7];
+	 msg->payload[9]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[8];
+	 msg->payload[10]              = schedule_vars.scheduleBuf[4].type;
+	 msg->payload[11]		  = schedule_vars.scheduleBuf[4].channelOffset;
+	 msg->payload[12]		  = schedule_vars.scheduleBuf[4].slotOffset
+	
+	 msg->payload[13]		  ='T';
+
+         msg->payload[14]               = schedule_vars.scheduleBuf[5].neighbor.addr_64b[0];
+	 msg->payload[15]               = schedule_vars.scheduleBuf[5].neighbor.addr_64b[1];
+	 msg->payload[16]               = schedule_vars.scheduleBuf[5].neighbor.addr_64b[2];
+         msg->payload[17]               = schedule_vars.scheduleBuf[5].neighbor.addr_64b[3];
+	 msg->payload[18]               = schedule_vars.scheduleBuf[5].neighbor.addr_64b[4];
+	 msg->payload[19]               = schedule_vars.scheduleBuf[5].neighbor.addr_64b[5];
+	 msg->payload[20]               = schedule_vars.scheduleBuf[5].neighbor.addr_64b[6];
+	 msg->payload[21]               = schedule_vars.scheduleBuf[5].neighbor.addr_64b[7];
+	 msg->payload[22]               = schedule_vars.scheduleBuf[5].neighbor.addr_64b[8];
+	 msg->payload[23]              = schedule_vars.scheduleBuf[5].type;
+	 msg->payload[24]		  = schedule_vars.scheduleBuf[5].channelOffset;
+         
+	 msg->payload[25]		  ='T';
+         */  		
+        
+	    //msg->payload[11]              = schedule_vars.debugPrintRow;
+	    //msg->payload[12]              = schedule_vars.maxActiveSlots;
+	    //msg->payload[13]              = neighbors_vars.neighbors[0].rssi;
 	   
 		
 
-         } else {
+         //} else {
 
 		
 
-	    msg->payload[1]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[0];
-	    msg->payload[2]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[1];
-	    msg->payload[3]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[2];
-            msg->payload[4]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[3];
-	    msg->payload[5]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[4];
-	    msg->payload[6]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[5];
-	    msg->payload[7]               =schedule_vars.scheduleBuf[4].neighbor.addr_64b[6];
-	    msg->payload[8]               =schedule_vars.scheduleBuf[4].neighbor.addr_64b[7];
-	    msg->payload[9]               =schedule_vars.scheduleBuf[4].neighbor.addr_64b[8];
-		
+	    //msg->payload[1]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[0];
+	    //msg->payload[2]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[1];
+	    //msg->payload[3]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[2];
+            //msg->payload[4]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[3];
+	    //msg->payload[5]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[4];
+	    //msg->payload[6]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[5];
+	    //msg->payload[7]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[6];
+	    //msg->payload[8]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[7];
+	    //msg->payload[9]               = schedule_vars.scheduleBuf[4].neighbor.addr_64b[8];
+	    //msg->payload[10]              = schedule_vars.scheduleBuf[4].type;
+	    //msg->payload[11]              = schedule_vars.debugPrintRow;
+	    //msg->payload[12]              = schedule_vars.maxActiveSlots;
+	    //msg->payload[13]              = neighbors_vars.neighbors[0].rssi;
         
 
 
@@ -114,7 +197,7 @@ owerror_t cleds_receive(
 
 
 
-         }
+         //}
             
          // set the CoAP header
          coap_header->Code                = COAP_CODE_RESP_CONTENT;
